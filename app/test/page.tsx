680b78be4ce35a05cfbfef2bd0fd667f9a28d4fa
isQHung <testkiller011@gmail.com>
2025-12-20 10:28:04 +0700

    new component mail-template

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import {
+    GroupInvitationEmail
+} from '@/components/ui/mail-template'
+
+export default function mail () {
+
+    return (
+        <GroupInvitationEmail UserName='isQ' GroupName='TEST'>
+        </GroupInvitationEmail>
+
+    )
+}
---
+++ components/ui/mail-template.tsx
@@ -0,0 +1,83 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components';
+import tailwindConfig from '@/tailwind.config';
+
+interface GroupInvitationEmailProps {
+  UserName: string;
+  GroupName: string
+}
+
+// const baseUrl = process.env.VERCEL_URL
+//   ? `https://${process.env.VERCEL_URL}`
+//   : '';
+
+export const GroupInvitationEmail = ({
+  UserName,
+  GroupName
+}: GroupInvitationEmailProps) => (
+  <Html>
+    <Head />
+    <Tailwind config={tailwindConfig}>
+      <Body className="bg-white font-koala">
+        <Preview>
+          The Intelligence ChatBot platform that helps you uncover qualified
+          leads.
+        </Preview>
+        <Container className="mx-auto py-5 pb-12">
+          <Img
+            src={`https://www.eaton.com/content/dam/eaton/products/electrical-circuit-protection/fuses/paul-p-gubany-center/bus-ele-power-short-circuit-capacity2.png`}
+            width="170"
+            height="50"
+            alt="logo"
+            className="mx-auto"
+          />
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
+            <h1 className='text-4xl font-bold text-slate-900 leading-tight'> Group Invitation</h1>
+          </div> 
+          
+          <Hr />
+          <Text className="text-[16px] leading-[26px]">
+            Hi {UserName},
+          </Text>
+          <Text className="text-[16px] leading-[26px]">
+            You are invited to Group {GroupName}.
+          </Text>
+          <Section className="text-center">
+            <Button
+              className="bg-[#5F51E8] rounded-[3px] text-white text-[16px] no-underline text-center block p-3"
+              href="localhost:3000/account/groups"
+            >
+              View Group
+            </Button>
+          </Section>
+          <Text className="text-[16px] leading-[26px]">
+            Best,
+            <br />
+            The ChatBot-HelpDesk team
+          </Text>
+          <Hr className="border-[#cccccc] my-5" />
+          <Text className="text-[#8898aa] text-[12px]">
+            footer
+          </Text>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>
+);
+
+GroupInvitationEmail.PreviewProps = {
+  UserName: 'Alan',
+} as GroupInvitationEmailProps;
+
+export default GroupInvitationEmail;
